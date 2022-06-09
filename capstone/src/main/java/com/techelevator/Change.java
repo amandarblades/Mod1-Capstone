8af57f979c50d6941d573dbfdeeb050ba26f5cd5
Amanda Blades <arhaskamp@gmail.com>
2022-06-09 12:44:41 -0400

    While loop update!

--- capstone/src/main/java/com/techelevator/Change.java
+++ capstone/src/main/java/com/techelevator/Change.java
@@ -10,15 +10,16 @@ public class Change extends Money {
     boolean isCurrentBalance = currentBalance >=100? true: false;
 
     int i = 1;
+    public void countDollars() {
+        while (i < 6) {
+            System.out.println("Hello World");
+            i++;
+        }
 
-    while(i < 6){
-        System.out.println("Hello World");
-        i++;
-    }
-
-    while (currentBalance > 99) {
-        dollars++;
-        currentBalance -=100;
+        while (currentBalance > 99) {
+            dollars++;
+            currentBalance -= 100;
+        }
     }
 
 
