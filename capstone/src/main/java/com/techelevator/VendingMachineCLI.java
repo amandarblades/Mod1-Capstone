5bb13bd61996e80061cac6a0a1abc61cde2886cf
Amanda Blades <arhaskamp@gmail.com>
2022-06-10 09:11:04 -0400

    Safety Push

--- capstone/src/main/java/com/techelevator/VendingMachineCLI.java
+++ capstone/src/main/java/com/techelevator/VendingMachineCLI.java
@@ -36,6 +36,7 @@ public class VendingMachineCLI {
 	public static void main(String[] args) {
 		Menu menu = new Menu(System.in, System.out);
 		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		
 		cli.run();
 	}
 }
